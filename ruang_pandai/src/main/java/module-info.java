fd0759ccb9107509c484edb6c579f59406e3077d
dzakyatha <18223124@std.stei.itb.ac.id>
2025-06-02 21:01:10 +0700

    refactor: add sqlite dependency in pom.xml and sql module in module-info.java

--- ruang_pandai/src/main/java/module-info.java
+++ ruang_pandai/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.ruang_pandai {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.ruang_pandai to javafx.fxml;
     exports com.ruang_pandai;
